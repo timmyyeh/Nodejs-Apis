09906b6dd5db6456cfd4093d389865b0e0348b7d
timmyyeh <timmyyeh@cs.washington.edu>
2020-02-19 23:32:00 -0800

    add register route

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const connectDB = require('./config/db');
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
 const app = express();
 
 connectDB();
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-    res.json("hello world");
+  res.json("hello world");
 });
 
-app.use('/api/register', require('./routes/register'));
+app.use("/api/register", require("./routes/register"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
-    console.log(`server is running at port: ${port}`)
-);
+app.listen(port, () => console.log(`server is running at port: ${port}`));
--- ecommerce/routes/register.js
+++ ecommerce/routes/register.js
@@ -1,13 +1,55 @@
-const mongoose = require('mongoose');
-const express = require('express');
+const express = require("express");
+const User = require("../models/users");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const {
+    check,
+    validationResult
+} = require("express-validator");
 
 const router = express.Router();
 
+router.get("/", (req, res) => res.send("register route"));
 
-router.get('/', (req, res) => res.send('register route'));
+router.post("/", [check("email").isEmail()], async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            });
+        }
 
-router.post('/', async (req, res) => {
+        const {
+            email,
+            password
+        } = req.body;
 
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({
+            email,
+            hashPassword
+        });
+
+        await user.save();
+
+        const token = jwt.sign({
+            data: {
+                'user': email
+            }
+        }, config.get("secret"), {
+            expiresIn: "1h",
+            algorithm: 'HS256'
+        });
+        return res.json({
+            token
+        });
+    } catch (error) {
+        return res.status(404);
+    }
 });
 
 module.exports = router;
